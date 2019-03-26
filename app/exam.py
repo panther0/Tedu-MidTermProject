ade6a31a520114a9b86832caf620b9a219b0183e
Henry.X <panther_0@163.com>
2019-03-26 14:09:28 +0800

    fix exam score

--- app/exam.py
+++ app/exam.py
@@ -1,5 +1,5 @@
 from flask import (
-    Blueprint, render_template, url_for, request, g
+    Blueprint, render_template, url_for, request, g, session
 )
 from . import db
 from .models import *
@@ -14,31 +14,44 @@ bp = Blueprint('exam',__name__,url_prefix='/exam')
 
 @bp.route('/exam', methods=['GET', 'POST'])
 def exam():
-    # TODO: 地址栏传入course
+    # # TODO: 地址栏传入course
     course = request.args.get("course")
     if request.method == 'POST':
-        # tests = db.session.query(Test).filter(Test.course == course).limit(10).all()
-        # l>数据库里的答案
-        l = []
-        # L>学生写的答案
-        L = []
-        for test in g.tests:
-            a = re.findall('A|B|C|D', test.answer)
-            l.append(a[0])
-            id = str(test.id)
-            L.append(request.form[id])
-        score = 0
+    #     # tests = db.session.query(Test).filter(Test.course == course).limit(10).all()
+    #     # l>数据库里的答案
+    #     l = []
+    #     # L>学生写的答案
+    #     L = []
+    #     for test in g.tests:
+    #         # a = re.findall('A|B|C|D', test.answer)
+    #         l.append(test.answer)
+    #         # l.append(a[0])
+    #         id = str(test.id)
+    #         L.append(request.form[id])
+    #     score = 0
 
-        for j in range(10):
-            # print(l[j], end=" ")
-            # print(L[j], end=" ")
-            if l[j] == L[j]:
+    #     for j in range(10):
+    #         # print(l[j], end=" ")
+    #         # print(L[j], end=" ")
+    #         if l[j] == L[j]:
+    #             score += 10
+        score = 0
+        list = request.form
+            
+        for t in list:
+            # print(t)
+            # print(list[t])
+            # idstr = str(t)
+            ans = db.session.query(Test).filter_by(id=t).one().answer
+            if list[t] == ans:
                 score += 10
         print(score)
 
-        return "考试结束"
+        return ("考试结束,您获得了%d分"%score)
     tests = db.session.query(Test).filter(Test.course == course).order_by(func.random()).limit(10).all()
-    g.tests = tests
+    # g.tests = tests
+    # session.tests = tests
+    # g.course = course
     return render_template('exam/exam.html', tests=tests,course=course)
 
 
